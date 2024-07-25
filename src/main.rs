1c5613dda030278e700102c2d74951b7db70ed66
V3D4NTH <vedanth.sameer@gmail.com>
2024-07-25 18:26:26 +0530

    Decode bencoded integers

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,34 @@
-use serde_json;
-use std::env;
+mod args;
+mod decode;
+use anyhow::Result;
+use args::Command;
 
-// Available if you need it!
-// use serde_bencode
+// Usage: your_bittorrent.sh decode "<encoded_value>"
+fn main() -> Result<()> {
+    let args = args::parse();
+    let Some(cmd) = args.command else {
+        anyhow::bail!("No command found.");
+    };
 
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+    match cmd {
+        Command::Decode { input } => handle_decode(&input),
     }
 }
 
-// Usage: your_bittorrent.sh decode "<encoded_value>"
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let command = &args[1];
+fn handle_decode(i: &str) -> Result<()> {
+    let Some(c) = i.chars().next() else {
+        anyhow::bail!("Input is empty");
+    };
 
-    if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        // println!("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
-        let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
+    if c == 'i' {
+        let i = decode::integer(i)?;
+        println!("{i}");
+    } else if c.is_ascii_digit() {
+        let s = decode::string(i)?;
+        println!("{s}");
     } else {
-        println!("unknown command: {}", args[1])
+        anyhow::bail!("Unknown encoded argument: {i}");
     }
+
+    Ok(())
 }
